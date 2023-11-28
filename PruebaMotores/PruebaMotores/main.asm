040a8dc2d9a021d0e0962eed9c2e374d5f0938ff
TFMFIUBA <tfernandez@fi.uba.ar>
2023-11-28 18:26:51 -0300

    Agrego comentarios del código fallando en PruebaMotores

--- PruebaMotores/PruebaMotores/main.asm
+++ PruebaMotores/PruebaMotores/main.asm
@@ -26,17 +26,7 @@ main:
 
   call configPuertos
   call configPWM
-  
-  ldi	r16, 0xff
-  
-  ;avanzar:
-  sts	OCR1BL, r16
-  sts	OCR1AL, r16
-  /*
-  ;reversa:
-  sts	OCR0B, r16
-  sts	OCR0A, r16
-  */
+
   loop:
   rjmp loop
 
@@ -49,10 +39,10 @@ configPuertos:
   ldi	r16, (1 << PD5) | (1 << PD6)
   out	DDRD, r16
   
-  /*
+  
   cbi PORTB, PB2
   cbi PORTB, PB1
-  */
+  
   cbi PORTD, PD5
   cbi PORTD, PD6
   
@@ -61,6 +51,8 @@ ret
 
 configPWM:
 
+   ldi r16, 0x00
+
   ;Configuración timer 1
   ldi	r16, (0 << WGM11) | (1 << WGM10) | (1 << COM1A1) | (0 << COM1A0) | (1 << COM1B1) | (0 << COM1B0)
   sts	TCCR1A, r16
@@ -68,14 +60,16 @@ configPWM:
   ldi	r16, (0 << WGM13) | (0 << WGM12) | (1 << CS12) | (0 << CS11) | (0 << CS10)
   sts	TCCR1B, r16
 
-  ldI r16, 0X00
+  ldI r16, 0X80
 
   sts	OCR1BH, r16
   sts	OCR1BL, r16
     
   sts	OCR1AH, r16
   sts	OCR1AL, r16
-  /*
+ /*
+  ldi r16, 0x00
+
   ;Configuración timer 0
   ldi	r16, (0 << WGM01) | (1 << WGM00) | (1 << COM0A1) | (0 << COM0A0) | (1 << COM0B1) | (0 << COM0B0)
   sts	TCCR0A, r16
@@ -83,9 +77,12 @@ configPWM:
   ldi	r16, (0 << WGM02) | (1 << CS02) | (0 << CS01) | (0 << CS00)
   sts	TCCR0B, r16
   
-   ldI r16, 0X00
+  ldI r16, 0X80
 
   sts	OCR0B, r16
   sts	OCR0A, r16
-  */
+ */
+
+  //SI DESCOMENTO LA CONFIGURACIÓN DEL TIMER 0 DEJA DE ANDAR EL TIMER 1
+  //CUANDO CONFIGURO EL TIMER 0 SE ACTIVA LA SALIDA B DEL TIMER 1
 ret
